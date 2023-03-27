b0a724afed81a61e12c877236d53cc9f0188b383
AngeloRoudani <ange.rouda@gmail.com>
2023-03-27 15:06:38 +0200

    navbar implementata

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+
+    <TheHeader/>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+
+</template>
+
+<script>
+
+    import TheHeader from './components/TheHeader.vue';
+
+    export default {
+        components : {
+            TheHeader
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,89 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="image_logo">
+                <img src="../assets/dc-logo.png" alt="logo dc">
+            </div>
+            <div class="link_list">
+                <ul>
+                    <li v-for="element in links"><a href="#">{{element}}</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+</template>
+
+<script>
+    export default {
+        name: "TheHeader",
+            data() {
+                return {
+                    links: [ 'characters', 'comics', 'movies', 'tv', 'games', 'collectibles', 'videos', 'fans', 'news', 'shops' ],
+                };
+            },
+        };
+</script>
+
+
+
+
+<style scoped lang="scss">
+
+header {
+    background-color: white;
+}
+
+.container {
+    width: 1170px;
+    height: 80px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.image_logo img {
+    height: 60px;
+}
+
+.link_list {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    ul {
+        height: 100%;
+        list-style: none;
+        text-transform: uppercase;
+        font-size: 12px;
+        font-weight: bold;
+        
+        li {
+            height: 100%;
+            padding: 0 1rem ;
+            display: inline-block;
+
+            li:hover {
+                border-bottom: solid 3px #0282f9;
+            }
+
+            a {
+                text-decoration: none;
+                color: black;
+                vertical-align: center;
+                padding-top: 30px;
+                display: inline-block;
+            }
+            a:hover {
+            color: #0282f9;
+            }
+        
+            
+        }
+        
+    }
+}
+
+
+
+</style>
